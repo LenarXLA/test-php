aa38a0d7b8eb01955cdfda2e7d3dae8052ca2e5c
Lenar <53114683+LenarXLA@users.noreply.github.com>
2021-01-06 16:41:54 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,45 @@
+$apiClient = new \AmoCRM\Client\AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
+
+$apiClientFactory = new \AmoCRM\AmoCRM\Client\AmoCRMApiClientFactory($oAuthConfig, $oAuthService);
+$apiClient = $apiClientFactory->make();
+
+$apiClient->setAccessToken($accessToken)
+->setAccountBaseDomain($accessToken->getValues()['baseDomain'])
+  ->onAccessTokenRefresh(
+    function (\League\OAuth2\Client\Token\AccessTokenInterface $accessToken, string $baseDomain) {
+      saveToken(
+        [
+          'accessToken' => $accessToken->getToken(),
+          'refreshToken' => $accessToken->getRefreshToken(),
+          'expires' => $accessToken->getExpires(),
+          'baseDomain' => $baseDomain,
+        ]
+      );
+});
+
+$leadsContacts = $apiClient->contacts();
+$leadsTasks = $apiClient->tasks();
+
+// Если у контакта нет сделок
+if (get(BaseEntityFilter $filter = null, array $leadsContacts = _embedded[leads]) == 0 || null) {
+  // Создадим коллекцию полей сущности
+  $leadCustomFieldsValues = new CustomFieldsValuesCollection();
+
+  // Создадим модель значений поля типа текст
+  $textCustomFieldValuesModel = new TextCustomFieldValuesModel();
+
+  // Укажем ID поля
+  $textCustomFieldValuesModel->setFieldId(123);
+
+  // Добавим значения
+  $textCustomFieldValuesModel->setValues(
+      (new TextCustomFieldValueCollection())
+          ->add((new TextCustomFieldValueModel())->setValue('Контакт без сделок'))
+  );
+
+  // Добавим значение в коллекцию полей сущности
+  $leadCustomFieldsValues->add($textCustomFieldValuesModel);
+
+  // Установим сущности эти поля
+  $leadsTasks->setCustomFieldsValues($leadCustomFieldsValues);
+};
